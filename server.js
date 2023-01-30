9dddfdd338f835e93666731661b7caa6e34c8b81
unknown <sankalprane1998@gmail.com>
2023-01-29 20:19:05 -0600

    cors policy changed

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const BFS_Service = require('./services/bfs-service').BFS_Service;
 
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.post('/bfs', (req, res) => {
     console.log(req.body);
     const bfs_service = new BFS_Service();
-    start =  [[5, 1, 2, 3], [9, 6, 7, 4], [13, 10, 11, 8], [0, 14, 15, 12]];
+    start =  req.body.start;
     res.json(bfs_service.start(start));
 });
 
-app.listen(3000);
+app.listen(4000);
